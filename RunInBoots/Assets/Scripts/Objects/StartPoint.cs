b65dd5b66b11073112397be3f04e2d05758c3ffa
2-0-is <yuye2002@snu.ac.kr>
2024-11-30 18:21:26 +0900

    [TASK-P45] Rollback annotations

--- RunInBoots/Assets/Scripts/Objects/StartPoint.cs
+++ RunInBoots/Assets/Scripts/Objects/StartPoint.cs
@@ -16,7 +16,7 @@ public class StartPoint : MonoBehaviour
 
         if (!GameManager.Instance.isRespawnPositionSetted)
         {
-            // �ʱ� ���� ��ġ�� ����
+            // 초기 스폰 위치를 설정
             Vector3 spawnPosition = transform.position + Vector3.up * 1.0f;
             GameManager.Instance.StartNewStage();
             //GameManager.Instance.SpawnPlayer(spawnPosition);
@@ -25,7 +25,7 @@ public class StartPoint : MonoBehaviour
         }
         else
         {
-            // ������ ��ġ�� �̹� ������ ��� ĳ���͸� ����
+            // 리스폰 위치가 이미 설정된 경우 캐릭터만 스폰
             // GameManager.Instance.SpawnPlayer(GameManager.Instance.respawnPosition);
             GameManager.Instance.SpawnPlayerWithEvent(GameManager.Instance.respawnPosition);
         }
