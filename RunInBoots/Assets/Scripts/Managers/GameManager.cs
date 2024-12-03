dc8861533dc25671db8ceb8a6ed3330558b72b9d
2-0-is <yuye2002@snu.ac.kr>
2024-12-03 11:57:49 +0900

    [TASK-P45] Implement each event content

--- RunInBoots/Assets/Scripts/Managers/GameManager.cs
+++ RunInBoots/Assets/Scripts/Managers/GameManager.cs
@@ -19,12 +19,33 @@ public class GameManager : MonoSingleton<GameManager>
         
     }
 
+    public void StartNewStageWithEvent(int stage)
+    {   
+        ProducingEvent stageClearEvent = new AnimatorEvent(null);
+        ProducingEvent blackScreenEvent = new AnimatorEvent(null);
+        GameObject goalPoint = GameObject.Find("AGoalPoint");
+        stageClearEvent.AddStartEvent(() =>
+        {
+            // Debug.Log("Stage Clear Event Start");
+            StopPlayer();
+            GetObject(goalPoint, new Vector3(0, 3.0f, 0));
+        });
+        blackScreenEvent.AddEndEvent(() =>
+        {
+            // Debug.Log("Stage Clear Event End");
+            ResumePlayer();
+            StartNewStage(stage);
+        });
+        AddEvent(stageClearEvent);
+        AddEvent(blackScreenEvent);
+    }
 
     public void StartNewStage(int stage)
     {
         if (!SceneLoader.LoadTargetStage(stage, 1))
         {
-            AllStageClear();
+            // AllStageClear();
+            AllStageClearWithEvent();
             return;
         }
         currentState = new StageState(stage);
@@ -69,16 +90,54 @@ public class GameManager : MonoSingleton<GameManager>
         }
     }
 
+    public void AllStageClearWithEvent()
+    {
+        ProducingEvent gameClearEvent = new AnimatorEvent(null);
+        ProducingEvent blackScreenEvent = new AnimatorEvent(null);
+        GameObject goalPoint = GameObject.Find("AGoalPoint");
+        gameClearEvent.AddStartEvent(() =>
+        {
+            // Debug.Log("Game Clear Event Start");
+            StopPlayer();
+            GetObject(goalPoint, new Vector3(0, 3.0f, 0));
+        });
+        blackScreenEvent.AddEndEvent(() =>
+        {
+            // Debug.Log("Game Clear Event End");
+            ResumePlayer();
+            AllStageClear();
+        });
+        AddEvent(gameClearEvent);
+        AddEvent(blackScreenEvent);
+    }
+
     // 모든 스테이지 클리어 된 후 동작 나중에 채워넣기
-    public void AllStageClear() 
+    private void AllStageClear() 
     {
         currentState = null;
         SceneLoader.LoadTitleScene();
         Debug.Log("Game Clear");
     }
 
+    public void GameOverWithEvent()
+    {
+        ProducingEvent gameOverEvent = new AnimatorEvent(null);
+        ProducingEvent blackScreenEvent = new AnimatorEvent(null);
+        gameOverEvent.AddStartEvent(() =>
+        {
+            // Debug.Log("GameOver Event Start");
+            StopPlayer();
+        });
+        blackScreenEvent.AddEndEvent(() =>
+        {
+            // Debug.Log("GameOver Event End");
+            GameOver();
+        });
+        AddEvent(gameOverEvent);
+        AddEvent(blackScreenEvent);
+    }
 
-    public void GameOver()
+    private void GameOver()
     {
         if (currentState != null)
         {
@@ -97,6 +156,46 @@ public class GameManager : MonoSingleton<GameManager>
         return currentState as StageState;
     }
 
+    public void SpawnPlayerWithEvent(Vector3 spawnPosition)
+    {
+        ProducingEvent spawnEvent = new AnimatorEvent(null);
+        spawnEvent.AddStartEvent(() =>
+        {
+            // Debug.Log("SpawnPlayerWithEvent Start");
+            GetCurrentStageState().SpawnPlayer(spawnPosition);
+            StopPlayer();
+        });
+        spawnEvent.AddEndEvent(() =>
+        {
+            ResumePlayer();
+            // Debug.Log("SpawnPlayerWithEvent End");
+        });
+        AddEvent(spawnEvent);
+    }
+
+    public void CollectCatnipWithEvent(int catnipID)
+    {
+        ProducingEvent catnipEvent = new AnimatorEvent(null);
+        GameObject player = GameObject.FindWithTag("Player");
+        GameObject catnip = GameObject.Find($"Catnip_{catnipID}");
+        catnipEvent.AddStartEvent(() =>
+        {
+            // Debug.Log("Catnip Event Start");
+            StopPlayer();
+            GetObject(catnip, new Vector3(0, 3.0f, 0));
+        });
+        catnipEvent.AddEndEvent(() =>
+        {
+            ResumePlayer();
+            MakePlayerInvincible();
+            GetCurrentStageState().CollectCatnipInStageState(catnipID);
+            catnip.SetActive(false);
+            // Debug.Log("Catnip Event End");
+        });
+        AddEvent(catnipEvent);
+    }
+
+    // Event Queue
     public void AddEvent(ProducingEvent producingEvent)
     {
         _eventQueue.Enqueue(producingEvent);
@@ -135,6 +234,15 @@ public class GameManager : MonoSingleton<GameManager>
             player.GetComponent<BattleModule>().enabled = true;
             // enable player action
             player.GetComponent<ActionSystem>().enabled = true;
+        }
+    }
+
+    public void MakePlayerInvincible()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        if(player != null)
+        {
+            player.GetComponent<BattleModule>().BeInvinvible();
         }
     }
 
--- RunInBoots/Assets/Scripts/Objects/StartPoint.cs
+++ RunInBoots/Assets/Scripts/Objects/StartPoint.cs
@@ -5,7 +5,8 @@ public class StartPoint : MonoBehaviour, ILevelObject
 {
     public void Initialize()
     {
-        GameManager.Instance.GetCurrentStageState().SpawnPlayer(transform.position);
+        // GameManager.Instance.GetCurrentStageState().SpawnPlayer(transform.position);
+        GameManager.Instance.SpawnPlayerWithEvent(transform.position);
         GameManager.Instance.GetCurrentStageState().UpdateRespawnPosition(transform.position, true);
         gameObject.SetActive(false);
     }
