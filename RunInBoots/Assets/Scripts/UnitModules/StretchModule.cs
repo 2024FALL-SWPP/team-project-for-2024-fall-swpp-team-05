3852d5337ad26d6b89740b325496a2aa285a6c58
paul159357 <42069232+paul159357@users.noreply.github.com>
2024-12-17 15:04:52 +0900

    [Issue-B29] 불안정한 콜라이더 사이즈를 Stretch 천장 체크서 배제
    init collider size 기반 ray 연산

--- RunInBoots/Assets/Scripts/UnitModules/StretchModule.cs
+++ RunInBoots/Assets/Scripts/UnitModules/StretchModule.cs
@@ -5,9 +5,6 @@ public class StretchModule : MonoBehaviour
     // Public variables for configuration
     public float maxStretchLength = 5f; // 최대 길이 (m)
     public float returnSpeed = 0.1f; // 돌아가기 속도 (Lerp 계수)
-    public float rayOffset = 0.5f;
-    public float rayError = 1.0f;
-    public float stretchSpeed = 50; // 늘리기 속도
     public Transform targetBone; // 상체 본 (이동시키기 위한 본)
 
     // Private variables to track stretching state
@@ -45,22 +42,13 @@ public class StretchModule : MonoBehaviour
     // **늘리기** 기능: 늘릴 길이만큼 스트레칭 적용
     public void Stretch(float stretchAmount)
     {
-        Debug.Log("Stretching: " + stretchAmount);
         // 천장 제한에 맞춰야 할 경우
         RaycastHit hit;
-        if (Physics.Raycast(transform.position+Vector3.up*(unitCollider.size.y-rayOffset), Vector3.up, out hit, stretchAmount+rayOffset, LayerMask.GetMask("Ground"))) {
-            Debug.Log("hit distance: " + hit.distance);
-            if (hit.distance > rayOffset) {
-                if (hit.distance - rayOffset < stretchAmount) {
-                    stretchAmount = hit.distance - rayOffset;
-                }
-                currentStretchAmount += stretchAmount * Time.deltaTime * stretchSpeed / 2;
-            }
-                
-        }
+        if (Physics.Raycast(transform.position+Vector3.up*(initialColliderSize.y+currentStretchAmount-0.1f), Vector3.up, out hit, stretchAmount+0.1f, LayerMask.GetMask("Ground")))
+            currentStretchAmount += hit.distance - 0.1f;
         // 늘리기 길이를 누적하고 최대 길이로 제한
         else
-            currentStretchAmount += stretchAmount * Time.deltaTime * stretchSpeed;
+            currentStretchAmount +=stretchAmount * Time.deltaTime * 60f;
 
         // 최대 길이 제한
         currentStretchAmount = Mathf.Clamp(currentStretchAmount, 0, maxStretchLength);
